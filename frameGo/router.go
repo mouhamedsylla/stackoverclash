7bdfd57c18b542efa67f15776757abdc9a9b39bf
Mouhamadou Sylla <syllamouhamed99@gmail.com>
2023-12-03 19:19:05 +0000

    clear

--- frameGo/router.go
+++ frameGo/router.go
@@ -22,48 +22,6 @@ func (r *Router) AddRoute(p string, v string) {
 	r.routes = append(r.routes, myRoute)
 }
 
-// func (r *Router) ResolvRoute() interface{} {
-// 	URL := js.Global().Get("location").Get("href").String()
-
-// 	element := js.Global().Get("document").Call("querySelector", "#App")
-// 	match := MatchPath(URL)
-// 	for i, route := range r.routes {
-// 		if route.path == match {
-// 			element.Set("innerHTML", route.view)
-// 			js.Global().Get("history").Call("pushState", js.Null(), "", route.path)
-// 			break
-// 		 } else if i == len(r.routes)-1 {
-// 			element.Set("innerHTML", "<h1>Error 404: Page Not Found")
-// 		}
-// 	}
-// 	return nil
-// }
-
-// func (r *Router) ResolvRoute() {
-	
-// 	location := js.Global().Get("location")
-// 	u := location.Get("href").String()
-// 	parsedURL := js.Global().Get("URL").New(u)
-// 	path := parsedURL.Get("pathname").String()
-
-// 	element := js.Global().Get("document").Call("querySelector", "#App")
-
-// 	match := false
-// 	for _, route := range r.routes {
-// 		if route.path == path {
-// 			match = true
-// 			r.renderView(element, route.view)
-// 			js.Global().Get("history").Call("pushState", js.Null(), "", route.path)
-// 			break
-// 		}
-// 	}
-
-// 	if !match {
-// 		r.renderView(element, "<h1>Erreur 404 : Page non trouvée</h1>")
-// 		js.Global().Get("history").Call("pushState", js.Null(), "", "/404")
-// 	}
-// }
-
 func (r *Router) ResolvRoute(URL string) interface{} {
 	element := js.Global().Get("document").Call("querySelector", "#clash")
 	match := MatchPath(URL)
@@ -78,10 +36,6 @@ func (r *Router) ResolvRoute(URL string) interface{} {
 	}
 	return nil
 }
-
-// func (r *Router) renderView(element js.Value, view string) {
-// 	element.Set("innerHTML", view)
-// }
 
 func MatchPath(url string) string {
 	pathRegex := regexp.MustCompile(`http://[^/]+(/.*)?`)
