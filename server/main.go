9c2bf27a1d62f4703d65da7b0f3c6f4399938ab6
Mouhamadou Sylla <syllamouhamed99@gmail.com>
2023-12-03 16:37:34 +0000

    progress bar added

--- server/main.go
+++ server/main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
-	"net/http"
 	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
 const port = ":8081"
 
 func main() {
-	static := http.FileServer(http.Dir("../assets/"))
+	spaHandler := SpaHandler("../assets", "index.html")
+	http.Handle("/", spaHandler)
 	log.Printf("Server started on http://localhost%s ...", port)
-	http.ListenAndServe(port, static)
+	http.ListenAndServe(port, nil)
+}
+
+type spaHandler struct {
+	publicDir string
+	indexFile string
+}
+
+func (h *spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	p := filepath.Join(h.publicDir, filepath.Clean(r.URL.Path))
+
+	if info, err := os.Stat(p); err != nil {
+		http.ServeFile(w, r, filepath.Join(h.publicDir, h.indexFile))
+		return
+	} else if info.IsDir() {
+		http.ServeFile(w, r, filepath.Join(h.publicDir, h.indexFile))
+		return
+	}
+
+	http.ServeFile(w, r, p)
+}
+
+func SpaHandler(publicDir string, indexFile string) http.Handler {
+	return &spaHandler{publicDir, indexFile}
 }
